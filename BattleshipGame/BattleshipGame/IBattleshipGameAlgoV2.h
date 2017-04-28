737c41baa83e362c72683fc03903a766f348d21d
Ofek Amir <ofekamir@mail.tau.ac.il>
2017-04-28 13:31:59 +0300

    Added predicted player Still need to think of how to recognize 2 players on 1 file

--- BattleshipGame/BattleshipGame/IBattleshipGameAlgoV2.h
+++ BattleshipGame/BattleshipGame/IBattleshipGameAlgoV2.h
@@ -9,18 +9,31 @@ enum class AttackResult {
 
 // IBattleshipGameAlgo for Ex2 - do not change this class - you should use it as is
 class IBattleshipGameAlgo {
+
 public:
+
 	virtual ~IBattleshipGameAlgo() = default;
-	virtual void setBoard(int player, const char** board, int numRows, int numCols) = 0;		// called once to notify player on his board
-	virtual bool init(const std::string& path) = 0;		// called once to allow init from files if needed returns whether the init succeeded or failed
-	virtual std::pair<int, int> attack() = 0;													// ask player for his move
-	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) = 0;	// notify on last move result
+
+	// called once to notify player on his board
+	virtual void setBoard(int player, const char** board, int numRows, int numCols) = 0;
+
+	// called once to allow init from files if needed 
+	// returns whether the init succeeded or failed
+	virtual bool init(const std::string& path) = 0;
+
+	// ask player for his move
+	virtual std::pair<int, int> attack() = 0;
+
+	// notify on last move result
+	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) = 0;
 };
 
-#ifdef ALGO_EXPORTS										// A flag defined in this project's Preprocessor's Definitions
-	#define ALGO_API extern "C" __declspec(dllexport)	// If we build - export
+#ifdef ALGO_EXPORTS // A flag defined in this project's Preprocessor's Definitions
+#define ALGO_API extern "C" __declspec(dllexport) // If we build - export
 #else
-	#define ALGO_API extern "C" __declspec(dllimport)	// If someone else includes this .h - import
+// If someone else includes this .h - import
+#define ALGO_API extern "C" __declspec(dllimport)
 #endif
 
-ALGO_API IBattleshipGameAlgo* GetAlgorithm();			// This method must be implemented in each player(algorithm) .cpp file
+// This method must be implemented in each player(algorithm) .cpp file
+ALGO_API IBattleshipGameAlgo* GetAlgorithm();
